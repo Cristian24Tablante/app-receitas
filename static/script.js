b68e13b737a203f5df532ccefd84504b98c276cf
CRISTIAN EMMANUEL TABLANTE MOGOLLON <cristian_mogollon@estudante.sesisenai.org.br>
2025-12-10 08:46:38 -0300

    actualizacao, tirei series e fimes e acrecentei a mudanca de senhas

--- static/script.js
+++ static/script.js
@@ -1,3 +1,20 @@
  function goBack() {
     window.history.back();
 }
+ // Funciones del dropdown de usuario
+    function menu_usuario() {
+        const dropdown = document.getElementById('user-dropdown');
+        dropdown.classList.toggle('show');
+    }
+
+    window.addEventListener('click', function(event) {
+        if (!event.target.matches('#user-icon-btn, #user-icon-btn img')) {
+            const dropdowns = document.getElementsByClassName("user-dropdown");
+            for (let i = 0; i < dropdowns.length; i++) {
+                const openDropdown = dropdowns[i];
+                if (openDropdown.classList.contains('show')) {
+                    openDropdown.classList.remove('show');
+                }
+            }
+        }
+    });
